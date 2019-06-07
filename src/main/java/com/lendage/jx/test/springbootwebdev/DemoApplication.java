b85bd669f06de9f7677a81706e5f1abba8ff4284
Roman Smolgovsky <rsmolgovsky@lendage.com>
2019-06-07 15:25:57 -0700

    Initial import

---
+++ src/main/java/com/lendage/jx/test/springbootwebdev/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.lendage.jx.test.springbootwebdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
